440c291d796d1849d61a0a914dffb0c662027475
Mosharraf Musa <mosharrafny@gmail.com>
2020-07-23 14:05:40 -0400

    User routers with authntication Done

--- api/server.js
+++ api/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const helmet = require("helmet");
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../routers/users/users-router");
 
 const server = express();
+const cors = require("cors");
 
+server.use(cors());
+server.use(helmet());
 server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("Welcome to Co-Make!");
 });
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 module.exports = server;
