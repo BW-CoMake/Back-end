271202ab043fd63055f568b9ca9c37a7764d19da
Mosharraf Musa <mosharrafny@gmail.com>
2020-07-24 20:24:37 -0400

    install pg

--- knexfile.js
+++ knexfile.js
@@ -23,10 +23,10 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL || {
-      database: "anything",
-      user: "user",
-      password: "pw",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
     },
     migrations: {
       directory: "./data/migrations",
