b7a0d3bbbd7ec950b7736cfa8687a719f2ec277d
Mosharraf Musa <mosharrafny@gmail.com>
2020-07-27 15:09:50 -0400

    added tablename to production variable

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -10,13 +10,13 @@ module.exports = (req, res, next) => {
   if (token) {
     jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
       if (err) {
-        console.log("token is:", token, "error is:", err);
+        // console.log("token is:", token, "error is:", err);
 
         res.status(401).json({ message: "Not a verified user" });
       } else {
         req.jwtToken = decodedToken;
-        console.log(" restricted secret.jwtsecret:", secret.jwtSecret);
-        console.log("decoded subject:", decodedToken.subject);
+        // console.log(" restricted secret.jwtsecret:", secret.jwtSecret);
+        // console.log("decoded subject:", decodedToken.subject);
 
         //anything running after this middleware can now use this req.jwtToken
         next();
--- routers/users-router.js
+++ routers/users-router.js
@@ -10,7 +10,7 @@ const router = express.Router();
 //GET all users for Co-Make
 
 router.get("/", restricted, (req, res) => {
-  console.log("req.jwtToken", req.jwtToken);
+  // console.log("req.jwtToken", req.jwtToken);
 
   Users.find()
     .then((users) => {
@@ -22,7 +22,7 @@ router.get("/", restricted, (req, res) => {
 //GET request users profile
 
 router.get("/:id", restricted, async (req, res) => {
-  console.log("req.jwtToken", req.jwtToken);
+  // console.log("req.jwtToken", req.jwtToken);
   const { id } = req.params;
   try {
     const getUser = await Users.findById(id);
@@ -41,7 +41,7 @@ router.get("/:id", restricted, async (req, res) => {
 
 router.put("/:id", restricted, validateUserUpdate, (req, res) => {
   const id = req.params.id;
-  console.log(id);
+  // console.log(id);
 
   Users.update(id, req.body)
     .then((user) => {
